b0847d4678ef46a00842de372d4421e61010331e
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-12-17 21:01:02 -0500

    🚀 add tlx deploy script

--- script/utils/parameters/OptimismParameters.sol
+++ script/utils/parameters/OptimismParameters.sol
@@ -4,5 +4,7 @@ pragma solidity 0.8.25;
 contract OptimismParameters {
     address constant OPTIMISM_KWENTA =
         0x920Cf626a271321C151D027030D5d08aF699456b;
+    address constant OPTIMISM_TLX =
+        0xD9cC3D70E730503E7f28c1B407389198c4B75FA2;
     address constant OPTIMISM_SNX = 0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4;
 }
